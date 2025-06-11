6fbedef7e97ceed27f392f14f1ff3c4d6990da07
Leela Satya SaiRam <214325905+LeelaSatyasairam@users.noreply.github.com>
2025-06-11 19:37:51 +0530

    edited vite.devfile

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,4 +8,10 @@ export default defineConfig({
   plugins: [react(),
      tailwindcss(),
   ],
+    build: {
+    outDir: 'dist'
+  },
+  server: {
+    historyApiFallback: true // this line is crucial for dev server
+  }
 })

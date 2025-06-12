9219ccf63b855b2a2ff97d70012c130cd7838a83
Leela Satya SaiRam <214325905+LeelaSatyasairam@users.noreply.github.com>
2025-06-12 10:00:32 +0530

    remove aschild in column section of category

--- frontend/src/pages/categorytable.jsx
+++ frontend/src/pages/categorytable.jsx
@@ -196,7 +196,7 @@ const handleEdit = (id) => {
           className="max-w-sm"
         />
         <DropdownMenu>
-          <DropdownMenuTrigger asChild>
+          <DropdownMenuTrigger>
             <Button variant="outline" className="ml-auto">
               Columns <ChevronDown />
             </Button>
